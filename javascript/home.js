eb9f7aa3a6d5fa1cb6e758c65a20da7b37e9700d
rakeshhatzade11 <hatzaderakesh1@gmail.com>
2024-01-14 19:08:53 +0530

    navbar and othe are in working

---
+++ javascript/home.js
@@ -0,0 +1,23 @@
+function changeImage(num){
+    const mainImage = document.getElementById('main-Product-Image');
+   
+    if(num==1){
+       mainImage.src = "./image/i61l9ppRIiqL._SX679_.jpg" ;
+    }
+    else if (num==2){
+       mainImage.src="./image/2.jpg";
+    }
+    else if (num==3){
+     mainImage.src="./image/3.jpg";
+   }
+   else if (num==4){
+       mainImage.src ="./image/4.jpg";
+   }
+   else if (num==5){
+       mainImage.src="./image/5.jpg"
+   }
+   else if (num==6){
+       mainImage.src="./image/6.jpg"
+   }
+   
+   }
