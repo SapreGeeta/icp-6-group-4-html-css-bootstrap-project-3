db0e2a0072485fed76626d4c0d3c59acb366b8b7
rakeshhatzade11 <hatzaderakesh1@gmail.com>
2024-01-15 19:01:47 +0530

    work in progress

--- javascript/home.js
+++ javascript/home.js
@@ -2,22 +2,22 @@ function changeImage(num){
     const mainImage = document.getElementById('main-Product-Image');
    
     if(num==1){
-       mainImage.src = "./image/i61l9ppRIiqL._SX679_.jpg" ;
+       mainImage.src="pro 1.webp" ;
     }
     else if (num==2){
-       mainImage.src="./image/2.jpg";
+       mainImage.src="pro2.webp";
     }
     else if (num==3){
-     mainImage.src="./image/3.jpg";
+     mainImage.src="pro 3.webp";
    }
    else if (num==4){
-       mainImage.src ="./image/4.jpg";
+       mainImage.src ="pro 4.webp";
    }
    else if (num==5){
-       mainImage.src="./image/5.jpg"
+       mainImage.src="pro 5.webp";
    }
    else if (num==6){
-       mainImage.src="./image/6.jpg"
+       mainImage.src="pro6.webp";
    }
    
    }
